cb508e058201a66733cd3c5df0443cac5279186c
Mateusz Kajzik <m.kajzik@gmail.com>
2021-02-17 19:18:09 +0100

    Changed slack and epic url to kube secret

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"kazik/free/game/integration/epic"
+	"os"
 )
 
 func main() {
 
-	webhookURL := readFromConfigurationFile("webhookURL")
-	epicURL := readFromConfigurationFile("epicURL")
+	webhookURL := os.Getenv("SLACK_URL")
+	epicURL := os.Getenv("EPIC_URL")
 
 	done := make(chan bool, 1)
 	go epic.CheckDailyGame(epicURL, webhookURL, done)
